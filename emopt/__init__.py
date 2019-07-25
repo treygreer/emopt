df0f92619d824bbebb7a03fe0b209b393159d3de
Trey Greer <tgreer@nvidia.com>
2019-07-25 13:18:08 -0400

    added template for cuda version of Grid

--- emopt/__init__.py
+++ emopt/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
-from . import adjoint_method, fdfd, fdtd, fdtd_cuda, fomutils, grid, io, misc, modes, optimizer, \
+from . import adjoint_method, fdfd, fdtd, fdtd_cuda, fomutils, grid, grid_cuda, io, misc, modes, optimizer, \
        simulation, geometry
 
-__all__ = ["adjoint_method", "fdfd", "fomutils", "grid", "io", "misc", "modes",
+__all__ = ["adjoint_method", "fdfd", "fomutils", "grid", "grid_cuda", "io", "misc", "modes",
           "optimizer", "fdtd", "fdtd_cuda", "simulation", "geometry"]
